ec809dd3414836394eb4757f8a5a622710f06400
Tim Wheeler <tim@timwheeler.com>
2018-08-28 19:42:43 -0400

    mosh 9

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+
+
+            // Chain methods
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char" + builder[0]);
+        }
+    }
+}
