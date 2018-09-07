ecaa7f620cbdfc7aa7228518cbc32475b1715915
Tim <tim@timwheeler.com>
2018-09-07 15:52:24 -0400

    mosh assignment

---
+++ Amazon/Amazon/Customer.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Amazon
+{
+
+    public class RateCalculator
+    {
+        public int Calculate(Customer customer)
+        {
+            return 0;
+        }
+    }
+    
+    
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+            
+            Console.WriteLine("Customer promoted.");
+        }
+        
+    }
+}
---
+++ _Inheritance/_Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _Inheritance
+{
+
+    public class GoldCustomer : Customer
+    {
+        public void OfferVoucher()
+        {
+            this.CalculateRating(exludeOrders: true);
+        }
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
