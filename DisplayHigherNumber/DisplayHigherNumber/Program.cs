fd66f367f4039e33cdf6518200468d270a7f9e95
Tim Wheeler <tim@timwheeler.com>
2018-09-11 19:06:06 -0400

    DisplayHigherNumber

---
+++ DisplayHigherNumber/DisplayHigherNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DisplayHigherNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var inputOne = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter another number: ");
+            var inputTwo = Convert.ToInt32(Console.ReadLine());
+
+            if (inputOne > inputTwo)
+            {
+                Console.WriteLine(inputOne);
+            }
+            else
+            {
+                Console.WriteLine(inputTwo);
+            }
+            
+        }
+    }
+}
