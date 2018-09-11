2f37d787633d73a28606632184d9e9669877bed1
Tim Wheeler <tim@timwheeler.com>
2018-09-11 19:44:31 -0400

    SpeedLimit

---
+++ SpeedLimit/SpeedLimit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SpeedLimit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the speed limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the vehicle speed: ");
+            var speed = Convert.ToInt32(Console.ReadLine());
+
+            var speedOver = speed - speedLimit;
+            
+            
+            
+            var demerits = Convert.ToInt32(speedOver / 5);
+            
+            if (speed <= speedLimit && speed > speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else if (speedOver > 0 && (demerits > 2))
+            {
+                Console.Write("Slow down! You have received ");
+                Console.Write(demerits);
+                Console.Write(" demerits!");
+            }
+        }
+    }
+}
