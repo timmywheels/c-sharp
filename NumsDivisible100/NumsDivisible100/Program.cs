86d56b0457ea1438b67c87d3d8062312f652515a
Tim Wheeler <tim@timwheeler.com>
2018-09-11 23:59:10 -0400

    sumInputNums

---
+++ NumsDivisible100/NumsDivisible100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumsDivisible100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 100;
+
+            var count = 0;
+
+            for (var i = 1; i <= num; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.Write("Count: ");
+            Console.Write(count);
+        }
+    }
+}
