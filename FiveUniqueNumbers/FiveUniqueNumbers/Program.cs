fa9963e5d5307d7999869b77c564b9a8c82da671
Tim <tim@timwheeler.com>
2018-09-13 21:00:09 -0400

    working on FiveUniqueNumbers

---
+++ FiveUniqueNumbers/FiveUniqueNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiveUniqueNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var uniqueNums = new List<string>();
+
+            var count = 0;
+
+            while (count <= 5)
+            {
+                
+                Console.Write("Enter a number: ");
+                var input = Console.Read();
+                
+                if (!uniqueNums.Contains(input.ToString()))
+                {
+                    uniqueNums.Add(input.ToString());
+                    ++count;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a unique number!");
+                }
+            }
+        }
+    }
+}
