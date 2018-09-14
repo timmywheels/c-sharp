77a5c3a035cea42b3216bc0af418e35b214c3528
Tim <tim@timwheeler.com>
2018-09-14 09:49:04 -0400

    added Number

---
+++ NumberUntilQuit/NumberUntilQuit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberUntilQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = String.Empty;
+
+            var numbers = new List<string>();
+            
+            do
+            {
+                
+                Console.Write("Enter a number: ");
+                input = Console.ReadLine();
+
+                if (input == "quit")
+                {
+                    
+                }
+                
+                else if (!numbers.Contains(input))
+                {
+                    numbers.Add(input);
+                }
+                
+                
+            } while (input != "quit");
+            
+            numbers.Sort();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");                
+            }
+        }
+    }
+}
